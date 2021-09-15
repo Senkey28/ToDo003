8e4cdbcb58eb281f6d6a5326e0efaf0f567fb365
Senkey <lera.leaf@gmail.com>
2021-09-15 22:25:14 +0300

    add solution N3

---
+++ ToDo003/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToDo003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
