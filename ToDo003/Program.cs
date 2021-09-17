0a4c8cecfd90a954670c36bc3bbe0dedb6850aa8
Senkey <lera.leaf@gmail.com>
2021-09-17 14:37:03 +0300

    Write fields and properties

--- ToDo003/Program.cs
+++ ToDo003/Program.cs
@@ -4,9 +4,13 @@ namespace ToDo003
 {
     class Program
     {
+        internal string putexs { get; set; }
+        internal string watchexs { get; set; }
+        internal bool statusexs { get; set; }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
